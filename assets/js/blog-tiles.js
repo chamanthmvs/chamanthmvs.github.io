5b62148dc2c47c20232145d1a2a8154c9d4bf734
Chamanth <mvschamanth@gmail.com>
2026-09-05 00:36:37 +0530

    Update blog tile creation with static test content
    Refactor blog tile creation to use static content and remove dynamic URL fetching.

--- assets/js/blog-tiles.js
+++ assets/js/blog-tiles.js
@@ -1,123 +1,28 @@
-const blogUrls = [
-    "https://mvsctechblogs.hashnode.dev/understanding-frameworks",
-    "https://mvschamanth.medium.com/what-does-backprop-over-time-in-rnn-mean-42333039d927c",
-    "https://medium.com/generative-ai/what-are-state-sategraph-and-workflow-in-langgraph-afc3f4392c6f"
-];
-
 const blogGrid = document.getElementById("blog-scroll");
 
-function createTile(url) {
+console.log("BLOG JS IS RUNNING");
+console.log("blogGrid:", blogGrid);
 
-    const tile = document.createElement("a");
+blogGrid.innerHTML = `
+    <a href="#" class="blog-tile">
 
-    tile.className = "blog-tile";
-    tile.href = url;
-    tile.target = "_blank";
-    tile.rel = "noopener noreferrer";
-
-    tile.innerHTML = `
         <div class="blog-tile-img"></div>
 
         <div class="blog-tile-body">
-            <span class="blog-tile-tag">Article</span>
-            <h3>Loading...</h3>
-            <p></p>
+
+            <span class="blog-tile-tag">
+                TEST ARTICLE
+            </span>
+
+            <h3>
+                This is a test blog
+            </h3>
+
+            <p>
+                If you can see this tile, your HTML and CSS are working.
+            </p>
+
         </div>
-    `;
 
-    blogGrid.appendChild(tile);
-
-    return tile;
-}
-
-
-async function loadTile(tile, url) {
-
-    try {
-
-        const response = await fetch(
-            "https://api.microlink.io/?url=" +
-            encodeURIComponent(url)
-        );
-
-        if (!response.ok) {
-            throw new Error("Microlink request failed");
-        }
-
-        const result = await response.json();
-
-        console.log("Microlink response:", result);
-
-        if (result.status !== "success") {
-            throw new Error("Microlink could not read the page");
-        }
-
-        const data = result.data;
-
-        const image =
-            data.image?.url ||
-            data.logo?.url ||
-            "";
-
-        tile.innerHTML = `
-            ${
-                image
-                ? `<img class="blog-tile-img" src="${image}" alt="">`
-                : `<div class="blog-tile-img"></div>`
-            }
-
-            <div class="blog-tile-body">
-
-                <span class="blog-tile-tag">
-                    ${data.publisher || "Article"}
-                </span>
-
-                <h3>
-                    ${data.title || "Untitled article"}
-                </h3>
-
-                <p>
-                    ${
-                        data.description
-                        ? data.description.slice(0, 100) + "..."
-                        : ""
-                    }
-                </p>
-
-            </div>
-        `;
-
-    } catch (error) {
-
-        console.error("Could not load:", url, error);
-
-        tile.innerHTML = `
-            <div class="blog-tile-img"></div>
-
-            <div class="blog-tile-body">
-
-                <span class="blog-tile-tag">
-                    Article
-                </span>
-
-                <h3>
-                    Unable to load preview
-                </h3>
-
-                <p>
-                    Click to read the article.
-                </p>
-
-            </div>
-        `;
-    }
-}
-
-
-blogUrls.forEach(url => {
-
-    const tile = createTile(url);
-
-    loadTile(tile, url);
-
-});
+    </a>
+`;
